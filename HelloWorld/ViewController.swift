9d0349917f938cb98ca7f3b286334af5944d6699
Rachel Pham <phamrachel17@yahoo.com>
2022-02-04 15:29:16 -0800

    Added Prework to repo

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Rachel Pham on 1/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func ButtonClicked(_ sender: Any) {
+        print("hello")
+        TextLabel.textColor = UIColor.orange
+    }
+    
+}
+
